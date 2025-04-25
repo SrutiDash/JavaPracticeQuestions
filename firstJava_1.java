76ce59142c2eddfe7af6b52b23e7febff7dbb697
Sruti Dash <sruti.dash1203@gmail.com>
2025-04-25 13:34:18 +0530

    even or odd by taking user input and if else statement

--- firstJava_1.java
+++ firstJava_1.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class firstJava_1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Write a program to print whether a number is even or odd, also take input from the user.
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a natural number: ");
+        int number = input.nextInt();
+        if(number==0){
+            System.out.println("please enter a natural number");
+        }else if(number%2==0){
+            System.out.println(number+" is even");
+        }else{
+            System.out.println(number+" is odd");
+        }
     }
 }
